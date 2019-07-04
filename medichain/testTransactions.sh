ed679be6bb92f251939ef3bbfd0eb9bf397b6d4c
Jay Parekh <jay.parekh0@gmail.com>
2019-07-04 23:10:08 +0530

    undo transaction changes

--- medichain/testTransactions.sh
+++ medichain/testTransactions.sh
@@ -162,9 +162,36 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
   }' 'http://localhost:3000/api/Package'
 
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
+     "packageId": "package4",
+     "name": "Heart Surgery Medical Term",
+     "description": "Long text describing procedure",
+     "category": "Cardiac",
+     "image": "BASE64",
+     "requiredDocs": ["MRI Scan", "CT Scan"],
+     "fundamentalProcedureNames": ["Heart Procedure 1", "Heart Procedure 2", "Heart Procedure 3"],
+     "fundamentalProcedureCosts": [5000, 75000, 5000],
+     "fundamentalProcedureTimes": ["1", "5", "10"],
+     "contingencyNames": ["Contingency 1", "Contingency 2", "Contingency 3"],
+     "contingencyDescriptions": ["Contingency Description 1", "Contingency Description 2", "Contingency Description 3"],
+     "contingencyCosts": [200, 600, 1000],
+     "contingencyTimes": ["2", "6", "10"],
+     "unvalidatedContingencyNames": [],
+     "unvalidatedContingencyDescriptions": [],
+     "unvalidatedContingencyCosts": [],
+     "unvalidatedContingencyTimes": [],
+     "totalFundamentalCost": 85000,
+     "totalFundamentalTime": "16",
+     "totalTime": "34",
+     "totalCost": 86800,
+     "providerId": "hospital1",
+     "providerType": "Hospital",
+     "providerName": "Some Hospital Name"
+}' 'http://localhost:3000/api/Package'
+
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{
      "$class": "medichain.network.BoughtPackage",
      "boughtPackageId": "boughtPackage1",
-     "packageId": "package1",
+     "packageId": "package4",
      "providerId": "hospital1",
      "providerType": "Hospital",
      "providerName": "Some Hospital Name",
